e21601c5057e7265b89f19594853e971e7a75045
Samuel Romero <sromerom@esliceu.net>
2020-07-29 17:17:12 +0200

    Passats el tests de numero a lletra

--- src/Numbers.java
+++ src/Numbers.java
@@ -37,6 +37,29 @@ public class Numbers {
             "Ninety"
     };
 
+    static String[] illions = {
+            "Zero",
+            "Million",
+            "Billion",
+            "Trillion",
+            "Quadrillion",
+            "Quintillion",
+            "Sextillion",
+            "Septillion",
+            "Octillion",
+            "Nonillion",
+            "Decillion",
+            "Undecillion",
+            "Duodecillion",
+            "Tredecillion",
+            "Quattuordecillion",
+            "Quindecillion",
+            "Sexdecillion",
+            "Septendecillion",
+            "Octodecillion",
+            "Novemdecillion"
+    };
+
     public static String say(long n) {
 
         String resultat = "";
@@ -48,8 +71,12 @@ public class Numbers {
             resultat = getDesenes(n);
         } else if (number.length() == 3) {
             resultat = getCentenes(n);
-        } else if (number.length() < 999999) {
+        } else if (n <= 999999) {
             resultat = getMilers(n);
+        } else if (n <= 999999999) {
+            resultat = getMillions(n);
+        } else {
+            resultat = getBillions(n);
         }
         return resultat;
     }
@@ -130,7 +157,7 @@ public class Numbers {
         if (centenaNumber != 0) {
 
             if (centenaNumber <= 99) {
-                resultat += getDesenes(centenaNumber).toLowerCase();
+                resultat += " " + getDesenes(centenaNumber).toLowerCase();
             } else {
                 resultat += " " + getCentenes(centenaNumber).toLowerCase();
             }
@@ -139,14 +166,150 @@ public class Numbers {
         return resultat;
     }
 
+    private static String getMillions(long n) {
+        String resultat = "";
+        int[] splitedNumbers = splitNumbers(n);
+        System.out.println(Arrays.toString(splitedNumbers));
+
+        if (splitedNumbers.length == 7) {
+            resultat = uniqueNumbers[splitedNumbers[0]] + " million";
+        } else if (splitedNumbers.length == 8) {
+            resultat = getDesenes(Integer.parseInt(Integer.toString(splitedNumbers[0]) + Integer.toString(splitedNumbers[1]))) + " million";
+        } else if (splitedNumbers.length == 9) {
+            resultat = getCentenes(Integer.parseInt(Integer.toString(splitedNumbers[0]) + Integer.toString(splitedNumbers[1]) + Integer.toString(splitedNumbers[2]))) + " million";
+        }
+
+        String stringMillionNumber = "";
+        for (int i = splitedNumbers.length - 6; i < splitedNumbers.length; i++) {
+
+            stringMillionNumber += Integer.parseInt(Integer.toString(splitedNumbers[i]));
+        }
+
+        int millionNumber = Integer.parseInt(stringMillionNumber);
+        //int millionNumber = Integer.parseInt(Integer.toString(splitedNumbers[1]) + Integer.toString(splitedNumbers[2]) + Integer.parseInt(Integer.toString(splitedNumbers[3])));
+        System.out.println(millionNumber);
+
+        if (millionNumber != 0) {
+
+            if (millionNumber <= 999) {
+                resultat += " " + getCentenes(millionNumber).toLowerCase();
+            } else {
+                resultat += " " + getMilers(millionNumber).toLowerCase();
+            }
+        }
+
+        return resultat;
+    }
+
+    private static String getBillions(long n) {
+        String resultat = "";
+        int[] splitedNumbers = splitNumbers(n);
+        System.out.println(Arrays.toString(splitedNumbers));
+
+        if (splitedNumbers.length == 10) {
+            resultat = uniqueNumbers[splitedNumbers[0]] + " billion";
+        } else if (splitedNumbers.length == 11) {
+            resultat = getDesenes(Integer.parseInt(Integer.toString(splitedNumbers[0]) + Integer.toString(splitedNumbers[1]))) + " billion";
+        } else if (splitedNumbers.length == 12) {
+            resultat = getCentenes(Integer.parseInt(Integer.toString(splitedNumbers[0]) + Integer.toString(splitedNumbers[1]) + Integer.toString(splitedNumbers[2]))) + " billion";
+        } else {
+
+            String number = Long.toString(n);
+            int illones = splitedNumbers.length;
+            boolean majus = true;
+            while (illones > 3) {
+                int[] a = splitNumbers(Long.parseLong(number));
+                illones = getSeparationBillion(Long.parseLong(number));
+                System.out.println(illones);
+                int init = number.length() - ((illones) * 3);
+
+                if (Long.parseLong(number) != n) {
+                    majus = false;
+                }
+
+                if (init == 1) {
+
+                    if (majus) {
+                        resultat += uniqueNumbers[a[0]] + " " + illions[illones - 1].toLowerCase();
+                    } else {
+                        resultat += uniqueNumbers[a[0]].toLowerCase() + " " + illions[illones - 1].toLowerCase();
+                    }
+                } else if (init == 2) {
+                    if (majus) {
+                        resultat += getDesenes(Integer.parseInt(Integer.toString(a[0]) + Integer.toString(a[1]))) + " " + illions[illones - 1].toLowerCase();
+                    } else {
+                        resultat += (getDesenes(Integer.parseInt(Integer.toString(a[0]) + Integer.toString(a[1]))) + " " + illions[illones - 1]).toLowerCase();
+                    }
+
+                } else if (init == 3) {
+                    if (majus) {
+                        resultat += getCentenes(Integer.parseInt(Integer.toString(a[0]) + Integer.toString(a[1]) + Integer.toString(a[2]))) + " " + illions[illones - 1].toLowerCase();
+                    } else {
+                        resultat += (getCentenes(Integer.parseInt(Integer.toString(a[0]) + Integer.toString(a[1]) + Integer.toString(a[2]))) + " " + illions[illones - 1].toLowerCase()).toLowerCase();
+                    }
+
+                }
+
+                number = "";
+                boolean red = true;
+                for (int i = init; i < a.length; i++) {
+                    if (a[i] != 0) {
+                        red = false;
+                    }
+                    number += Integer.toString(a[i]);
+                }
+
+                if (illones != 3 && !red) {
+                    resultat += " ";
+                }
+            }
+        }
+
+        String stringMillionNumber = "";
+        for (int i = splitedNumbers.length - 9; i < splitedNumbers.length; i++) {
+
+            stringMillionNumber += Integer.parseInt(Integer.toString(splitedNumbers[i]));
+        }
+
+        int millionNumber = Integer.parseInt(stringMillionNumber);
+        //int millionNumber = Integer.parseInt(Integer.toString(splitedNumbers[1]) + Integer.toString(splitedNumbers[2]) + Integer.parseInt(Integer.toString(splitedNumbers[3])));
+        System.out.println(millionNumber);
+
+        if (millionNumber != 0) {
+
+            if (millionNumber <= 999999) {
+                resultat += " " + getMilers(millionNumber).toLowerCase();
+            } else {
+                resultat += " " + getMillions(millionNumber).toLowerCase();
+            }
+        }
+
+        return resultat;
+    }
+
 
     private static int[] splitNumbers(long n) {
-        String number = Integer.toString((int) n);
+        String number = Long.toString(n);
+
         int[] ar = new int[number.length()];
         for (int i = 0; i < ar.length; i++) {
             ar[i] = Integer.parseInt(Character.toString(number.charAt(i)));
         }
         return ar;
+    }
+
+    private static int getSeparationBillion(long n) {
+        String number = Long.toString(n);
+        int aux = 0;
+        int resultat = 0;
+        for (int i = 0; i < number.length(); i++) {
+            aux++;
+            if (aux == 3 && i != number.length() - 1) {
+                resultat++;
+                aux = 0;
+            }
+        }
+        return resultat;
     }
 
 }
